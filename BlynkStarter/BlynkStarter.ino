967af1e9cf791625cdceb5e04d87e41fb8bcf291
stephendov <40470740+stephendov@users.noreply.github.com>
2020-03-03 22:18:59 -0600

    Report and Code

--- BlynkStarter/BlynkStarter.ino
+++ BlynkStarter/BlynkStarter.ino
@@ -39,24 +39,93 @@
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "Your token here";
+char auth[] = "SL_lq_UURqdy75TnsfhszE2eGDk5fLdn";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 // The EE IOT network is hidden. You might not be able to see it.
 // But you should be able to connect with these credentials. 
-char ssid[32] = "EE-IOT-Platform-02";
-char pass[32] = "g!TyA>hR2JTy";
+char ssid[32] = "OnePlus 6";
+char pass[32] = "3a5865c677c8";
+int LEDState = 0;
+
+#define LED 2
+const int freq = 5000;     // 5KHz frequency is more than enough. Remember we used 100 before.
+const int ledChannel = 0;  // You can have up to 16 pwm channels (0 to 15)
+const int resolution = 10; // 10 bit resolution for 1023. Freq should be greater than resolution.
+int dutyCycle = 0;
+int uptime = 0;
+
+int time_count = 0; // timer counter global variable
+String content = "";  // null string constant ( an empty string )
+
+BlynkTimer timer;
 
 void setup()
 {
   // Serial Monitor
   Serial.begin(115200);
   Blynk.begin(auth, ssid, pass);
+
+  // configure LED PWM functionality
+  ledcSetup(ledChannel, freq, resolution);
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(LED, ledChannel);
+
+  timer.setInterval(1000L, myTimerEvent);
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run();
 }
 
+BLYNK_WRITE(V1)
+{
+
+    // param is a member variable of the Blynk ADT. It is exposed so you can read it.
+    int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+
+    // Because V1 is a button, pinValue will be a 0 or a 1.
+    if (pinValue == 0) {
+        // turn LED off
+        LEDState = 0;
+        ledcWrite(ledChannel, 0);
+    }
+    else {
+        // turn LED on
+        LEDState = 1; 
+        ledcWrite(ledChannel, dutyCycle);
+    }
+}
+
+BLYNK_WRITE(V3)
+{
+
+    // param is a member variable of the Blynk ADT. It is exposed so you can read it.
+    int pinValue = param.asInt(); // assigning incoming value from pin V3 to a variable
+    dutyCycle = pinValue;
+    if(LEDState > 0){
+    ledcWrite(ledChannel, dutyCycle);
+    } 
+}
+
+void myTimerEvent()
+{
+// You can send any value at any time.
+// Don't send more than 10 values a second or the Blynk server will block you!
+  if (uptime % 25 == 0) {
+      char character; 
+      while(Serial.available()) { // Check if serial is available every 10 ms
+      character = Serial.read();
+      content.concat(character);
+      }
+      if (content != "") {
+        Blynk.virtualWrite(V2, content);
+        content = ""; // Clear String
+      }  
+  }
+  uptime += 1;
+  Blynk.virtualWrite(V0, millis() / 1000); // Write the arduino uptime every second
+}
--- Report.md
+++ Report.md
@@ -8,36 +8,40 @@ Team Number:
 
 1. What is the purpose of an IP address?
 
-    answer
+    To identify every machine accessing a netowrk.
 
 2. What is a DNS? What are the benefits of using domain names instead of IP addresses?
 
-    answer
+    A DNS maps the IP adress' of the machines on LAN to a symbolic name
 
 3. What is the difference between a static IP and a dynamic IP?
 
-    answer
+    A static IP doesn't change as a machine connects over time from different locations, while dynamc IP's change
+    based on network connnection.
 
 4. What is the tradeoff between UDP and TCP protocols?
 
-    answer
+    UDP sends the data at the fastest speed possible without concern for integrity, and TCP sends data with emphasis 
+    on data integrity not speed.
 
 5. Why can't we use the delay function with Blynk?
 
-    answer
+    Blynk obscures the data floe from us, so we can't use a delay function as it would ruin the timing of the system.
 
 6. What does it mean for a function to be "Blocking"?
 
-    answer
+    A function is blocking if it prevents other segments of code from executing while it is running
 
 7. Why are interrupts useful for writing Non-Blocking code?
 
-    answer
+    Interrupts are good for non-blocking code because it allows for the PC to be moved at any time allowing for 
+    execution of other instructions
 
 8. What is the difference between interface and implementation? Why is it important?
 
-   answer
+    Interface is how a function is called, but implemtation is the code that allows the function to perform a task.
+    This is important because a function can be called properly, but its implementation can be wrong leading to errors.
 
 9. Screenshot of your Blynk App:
 
-    ![your image here->](img/placeholder.png)
+    ![your image here->](img/Lab2_Screenshot.jpg)
